40c229af4b30127accb26cfb64c43be0d535443d
wenqing_i <wenqing_1@didiglobal.com>
2019-05-28 15:00:27 +0800

    vue知识积累

---
+++ vue-test/src/js/main.js
@@ -0,0 +1,8 @@
+	// 导入jquery类库
+    import $ from 'jquery'
+
+    // 设置偶数行背景色，索引从0开始，0是偶数
+    $('#list li:even').css('backgroundColor','lightblue');
+    // 设置奇数行背景色
+    $('#list li:odd').css('backgroundColor','pink');
+
